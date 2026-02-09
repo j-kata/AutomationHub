c4a9aacfaa213a70ecb10c2f244fbffbd01ecea1
katja tsareva <katja.tcareva@gmail.com>
2026-02-09 23:09:24 +0100

    Add npgsql connection options

--- AutomationHub/Infrastructure/Extensions/ApplicationContextExtenstions.cs
+++ AutomationHub/Infrastructure/Extensions/ApplicationContextExtenstions.cs
@@ -8,7 +8,19 @@ public static class ApplicationContextExtensions
     public static IServiceCollection AddApplicationContext(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"),
+                npgsqlOptions => 
+                {
+                    npgsqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 3,
+                        maxRetryDelay: TimeSpan.FromSeconds(5),
+                        errorCodesToAdd: null
+                    );
+                    npgsqlOptions.CommandTimeout(30);
+                }
+            )
+        );
 
         return services;
     }
